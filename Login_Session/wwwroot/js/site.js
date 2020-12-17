ce1f05cc0c6ae0993f3c97e0ef38107b4ffda2af
charl <charl@DESKTOP-AFJULCV>
2020-12-17 20:23:47 +0000

    -Generate workout page is fully functional (currently shows all exercises in the exercise table under the exercise areas specified by the user, will look to change this to two random exercises for each exercisearea selected in the next push to git) -Choose workout area page has been ammended to generate the index of the entry it creates within the workout table that is then passed to the generate workout page to be used within SQL queries -Database connection string has been ammended to allow for multiple SQL queries per razor page -Have begun looking to display a JS alert window if the user doen't select any workout areas and submits this -Databases still won't stage/commit/push due to current update, feel free to email me if you need me to send any of them over to you

--- Login_Session/wwwroot/js/site.js
+++ Login_Session/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function NoExercisesSelected()
+{
+    alert("INVALID: No Exercises Selected");
+}
